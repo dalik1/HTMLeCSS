f4436c0caea0e45cd95c82cf5cf805e9e658e5df
Mateus S. Oliveira <mateusmateu17@gmail.com>
2022-04-17 15:20:14 -0300

    17.4

--- faculdade/al003/ex1.php
+++ faculdade/al003/ex1.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>introduçao php</title>
-</head>
-<body>
-    <?php
-        $data_de_hoje = date ("d/m/y", time())
-    ?>
-</body>
+
+<html>
+    <body>
+        <?php
+            $data_de_hoje = date ("d/m/Y",time());
+        ?>
+        <p aling ="center"> hoje é dia <br><?php echo $data_de_hoje; ?></p>
+    </body>
 </html>
